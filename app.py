23119cdcd85bfb85d397d33a3675d2d5d54af7d5
Shrinidhi <ballalshrinidhi@gmail.com>
2023-11-08 01:02:09 +0530

    initial

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ application with the following capabilities
 
 # UUID of the non owner accessory service (TODO)
 UUID = '00000090-0100-0000-0000-000000000000'
-
+indication = ['']
 
 def indication_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
     """
@@ -26,114 +26,114 @@ def indication_handler(characteristic: BleakGATTCharacteristic, data: bytearray)
     2. Command Response (during sound start, stop operations)
     3. Get Serial Number Response
     """
-
+    indication[0] = ''
+    value = ''
     opcode = int.from_bytes(data[:2], 'big')
     data = data[2:]
     if opcode in helper.accessory_information:
         data = data.decode()
         value = helper.callbacks[opcode](data)
         try:
-            print(
-                f'[INDICATION] {helper.accessory_information[opcode]}: {value}', flush=True)
+            value += f'[INDICATION] {helper.accessory_information[opcode]}: {value}<br><br>'
         except Exception as e:
-            print(f"ERROR while interpreting {opcode}: {e}", flush=True)
+            value += f"ERROR while interpreting {opcode}: {e}<br><br>"
     elif opcode == opcodes.COMMAND_RESPONSE:
-        print('[INDICATION] ', helper.command_response_handler(data), flush=True)
+        value += '[INDICATION] '+ helper.command_response_handler(data) + '<br><br>'
     elif opcode == opcodes.GET_SERIAL_NUMBER_RESPONSE:
-        print('Serial Number Lookup URL (open in browser): ',
-              helper.serial_number_handler(data), flush=True)
-
-
-devices = {}
-devices_list = []
-
+        value += 'Serial Number Lookup URL (open in browser): '+helper.serial_number_handler(data) + '<br><br>'
+    indication[0] += value
 
 def callback(device, advertisement_data):
     if device not in devices:
         devices[device] = advertisement_data
 
+from flask import Flask, render_template, jsonify, request
 
-async def main():
-    """
-    Main method
-    - Scans for bluetooth devices
-    - Connects to the specified accessory
-    """
-    while True:
-        devices.clear()
-        scanner = BleakScanner(callback)
-        print("Scanning for devices...")
-        await scanner.start()
-        await asyncio.sleep(15)
-        await scanner.stop()
-        devices_list = list(devices)
+app = Flask(__name__)
+devices = {}
+addresses = ['']
 
-        for i, device in enumerate(devices_list):
-            print(f"{i + 1}. {device}")
+@app.route('/')
+def index():
+    return render_template('index.ejs')
 
-        address = ""
-        while True:
-            print("\nEnter the index of device to connect to (0 to rescan): ")
-            choice = input()
+@app.route('/start', methods=['POST'])
+async def start():
+    addresses = ['']
+    devices.clear()
+    scanner = BleakScanner(callback)
+    data = ''
+    await scanner.start()
+    await asyncio.sleep(15)
+    await scanner.stop()
+    devices_list = list(devices)
+    for i, device in enumerate(devices_list):
+        data += f"{i + 1}. {device}<br><br>"
+    return jsonify({'data': data})
+
+@app.route('/search', methods=['POST'])
+async def search():
+    choice = request.form.get('number')
+    devices_list = list(devices)
+    try:
+        choice_idx = int(choice)
+        if choice_idx >= 1 and choice_idx <= len(devices_list):
+            address = devices_list[choice_idx - 1].address
+    except Exception:
+        address = ''
+    addresses[0] = address
+    data = ''
+    data += f"Connecting to {address}...<br><br>"
+    try:
+        async with BleakClient(address, timeout=40) as client:
+            data += f"Connected to device {address}<br><br>"
+            await client.start_notify(UUID, indication_handler)
+            await asyncio.sleep(3)
             try:
-                choice_idx = int(choice)
-                if choice_idx >= 1 and choice_idx <= len(devices_list):
-                    address = devices_list[choice_idx - 1].address
-                    break
-                elif choice_idx == 0:
-                    break
-                else:
-                    print("Please enter a valid index")
-            except Exception:
-                print("Please enter a valid index")
-        if address != "":
-            break
-    while True:
-        print(f"Connecting to {address}...")
-        try:
-            async with BleakClient(address, timeout=40) as client:
-                print(f"Connected to device {address}")
-                await client.start_notify(UUID, indication_handler)
-                await asyncio.sleep(3)
-                try:
-                    print("Fetching accessory information..")
-                    for opcode in [opcodes.GET_PRODUCT_DATA, opcodes.GET_MANUFACTURER_NAME, opcodes.GET_MODEL_NAME, opcodes.GET_ACCESSORY_CATEGORY, opcodes.GET_ACCESSORY_CAPABILITIES]:
-                        opcode_in_bytes = opcode.to_bytes(2, 'big')
-                        await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
+                data += "Fetching accessory information..<br><br>"
+                for opcode in [opcodes.GET_PRODUCT_DATA, opcodes.GET_MANUFACTURER_NAME, opcodes.GET_MODEL_NAME, opcodes.GET_ACCESSORY_CATEGORY, opcodes.GET_ACCESSORY_CAPABILITIES]:
+                    opcode_in_bytes = opcode.to_bytes(2, 'big')
+                    await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
+            except KeyboardInterrupt:
+                data += "Disconnecting and exiting program<br><br>"
+                await client.disconnect()
+    except Exception as e:
+        data += f"ERROR: Could not connect to {address}, {e}<br><br>"
+    return jsonify({'data': data})
 
-                    while True:
-                        print("1. Sound Start")
-                        print("2. Sound Stop")
-                        print("3. Serial Number Lookup over BLE")
-                        print("4. Exit")
-                        inp = input("Enter your choice: ")
-                        try:
-                            if inp == "1":
-                                opcode_in_bytes = opcodes.SOUND_START.to_bytes(
-                                    2, 'big')
-                                await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
-                            elif inp == "2":
-                                opcode_in_bytes = opcodes.SOUND_STOP.to_bytes(
-                                    2, 'big')
-                                await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
-                            elif inp == "3":
-                                opcode_in_bytes = opcodes.GET_SERIAL_NUMBER.to_bytes(
-                                    2, 'big')
-                                await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
-                            elif inp == "4":
-                                raise KeyboardInterrupt
-                            print(flush=True)
-                        except Exception as e:
-                            print("Error while executing operation: ", e)
-                except KeyboardInterrupt:
-                    print("Disconnecting and exiting program")
-                    await client.disconnect()
-                    exit(0)
-        except Exception as e:
-            print(f"ERROR: Could not connect to {address}, {e}")
-            print(f"Do you want to retry connecting to {address} (y/n)?")
-            choice = input()
-            if choice != "y":
-                break
+@app.route('/button_click', methods=['POST'])
+async def button_click():
+    button_id = request.form.get('button_id')
+    data = ''
+    if button_id == "button4":
+        return jsonify({'data': ''})
+    try:
+        async with BleakClient(addresses[0], timeout=40) as client:
+            data += f"Connected to device {addresses[0]}<br><br>"
+            await client.start_notify(UUID, indication_handler)
+            await asyncio.sleep(3)
+    except Exception as e:
+        data += f"\nERROR: Could not connect to address, {e}"
+        return jsonify({'data': data})
+    try:
+        if button_id == "button1":
+            opcode_in_bytes = opcodes.SOUND_START.to_bytes(
+                2, 'big')
+            await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
+        elif button_id == "button2":
+            opcode_in_bytes = opcodes.SOUND_STOP.to_bytes(
+                2, 'big')
+            await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
+        elif button_id == "button3":
+            opcode_in_bytes = opcodes.GET_SERIAL_NUMBER.to_bytes(
+                2, 'big')
+            await client.write_gatt_char(UUID, opcode_in_bytes, response=True)
+        await asyncio.sleep(15)
+        data += indication[0]
+        print(flush=True)
+    except Exception as e:
+       data += "\nError while executing operation: "+e+'<br><br>'
+    return jsonify({'data': data})
 
-asyncio.run(main())
+if __name__ == '__main__':
+    app.run(debug=True)
